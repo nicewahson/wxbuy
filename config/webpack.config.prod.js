59a5faf8051c90df26dd92da21befbd8886802d1
nicewahson <nicelj92@gmail.com>
2017-12-15 11:31:53 +0800

    z

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -65,7 +65,7 @@ module.exports = {
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     // publicPath: publicPath,
-      publicPath: '/wxpurchase/wxcenter/build',
+      publicPath: '/wxpurchase/wxcenter/build/',
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info =>
       path
