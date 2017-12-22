537dcda85f319d7a1b6992efc27e51be2f3d3df6
wanglong <24137075@qq.com>
2017-12-22 10:32:01 +0800

    wl

---
+++ public/ma/ma.js
@@ -0,0 +1,44 @@
+(function () {
+    var params = {};
+    //Document��������
+    if(document) {
+        params.domain = document.domain || ''; 
+        params.url = document.URL || ''; 
+        params.title = document.title || ''; 
+        params.referrer = document.referrer || ''; 
+    }   
+    //Window��������
+    if(window && window.screen) {
+        params.sh = window.screen.height || 0;
+        params.sw = window.screen.width || 0;
+        params.cd = window.screen.colorDepth || 0;
+    }   
+    //navigator��������
+    if(navigator) {
+        params.lang = navigator.language || ''; 
+    }   
+    //����_maq����
+    if(_maq) {
+        for(var i in _maq) {
+            switch(_maq[i][0]) {
+                case '_setAccount':
+                    params.account = _maq[i][1];
+                    break;
+                default:
+                    break;
+            }   
+        }   
+    }   
+    //ƴ�Ӳ�����
+    var args = ''; 
+    for(var i in params) {
+        if(args != '') {
+            args += '&';
+        }   
+        args += i + '=' + encodeURIComponent(params[i]);
+    }   
+ 
+    //ͨ��Image���������˽ű�
+    var img = new Image(1, 1); 
+    img.src = 'http://activity-server.show.sanqimei.com/visit/gif?' + args;
+})();
